be4adf7ea637f9f3c8cb311aa3f2c94686052aa7
Mansi Negi <Mansi Negi>
2025-04-10 10:32:48 +0530

    added login and signup page

--- client/src/components/SignUp/Signup.jsx
+++ client/src/components/SignUp/Signup.jsx
@@ -5,15 +5,15 @@ function Login() {
         <>
             <div className=" w-full flex-row mt-20">
                 <div className=" flex justify-center w-full h-2/3">
-                    <h2>LogSignup</h2>
+                    <h2>Signup</h2>
                 </div>
                 <div className="w-full flex justify-center ">
                     <form className="border-2 border-gray-700 bg-gray-400 w-1/2 p-3  ">
 
                         <div className="">
-                            <label for="exampleInputName1" className="form-label">Email address</label>
-                            <input type="email" className="form-control" id="exampleInputName1" aria-describedby="NameHelp" />
-                            <div id="NameHelp" className="form-text">We'll never share your email with anyone else.</div>
+                            <label for="exampleInputName1" className="form-label">Full Name</label>
+                            <input type="text" className="form-control" id="exampleInputName1" aria-describedby="NameHelp" />
+                            <div id="NameHelp" className="form-text"></div>
                         </div>
                         <div className="">
                             <label for="exampleInputEmail1" className="form-label">Email address</label>
@@ -26,7 +26,7 @@ function Login() {
                         </div>
 
                         <button type="submit" className="btn btn-primary mt-3">Register</button>
-                       
+                        <p>Already a member? <Link to="/" className="underline">Login</Link></p>
                     </form>
                 </div>
             </div>
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,11 +4,13 @@ import './index.css'
 import { createBrowserRouter, createRoutesFromElements, Route,RouterProvider } from 'react-router-dom'
 import Layout from './Layout.jsx'
 import Login from './components/Login/Login.jsx'
+import Signup from "./components/SignUp/Signup.jsx"
 
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route path="" element={<Layout/>}>
+    <Route path="/" element={<Layout/>}>
       <Route path='/'element={<Login/>} />
+      <Route path='signup'element={<Signup/>} />
     </Route>
   )
 )
